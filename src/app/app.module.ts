6f4f0455a6cc3e08c70d36a68045f623a139142d
Hugo Heuts <hugoheus@gmail.com>
2020-01-31 13:36:31 +0100

    Added modules and DatePipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AstronautOverviewComponent } from './components/astronaut-overview/astronaut-overview.component';
 import { HttpClientModule } from '@angular/common/http';
-import { AlertModule } from 'ngx-bootstrap';
+import { AlertModule, AccordionModule, CarouselModule } from 'ngx-bootstrap';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './components/home/home.component';
+import { DatePipe } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
     AstronautOverviewComponent,
+    NavBarComponent,
     HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
     AlertModule.forRoot(),
+    AccordionModule.forRoot(),
+    CarouselModule.forRoot()
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
