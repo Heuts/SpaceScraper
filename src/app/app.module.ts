a1e34f8e5164583344f2888b977992d21ad78a0b
Hugo Heuts <hugoheuts@gmail.com>
2020-02-22 16:48:02 +0100

    Removed old unused bootstrap imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AstronautOverviewComponent } from './components/astronaut-overview/astronaut-overview.component';
 import { HttpClientModule } from '@angular/common/http';
-import { AccordionModule, CarouselModule } from 'ngx-bootstrap';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NasaCarousel } from './components/nasa-carousel/nasa-carousel.component';
